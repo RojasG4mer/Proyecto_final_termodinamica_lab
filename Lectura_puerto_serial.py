967e79803a70620a31e69e6bb6456f3425e70056
rojasg4mer <jonathan.frmipn@gmail.com>
2023-11-15 15:37:04 -0600

    Comentarios al codigo y modificacion final

--- Lectura_puerto_serial.py
+++ Lectura_puerto_serial.py
@@ -9,9 +9,8 @@ import serial
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-# Abre el archivo para escritura
-archivo = open('C:\\Users\\jonat.JONATHANROJAS\\Desktop\\SENSOR_temperatura\\prueba.txt', 'w')
+#Creamos el dataframe para la lectura de los datos
+df = pd.DataFrame(columns = ['Tiempo', 'Temperatura'])
 
 # Abre el puerto serial
 serialArduino = serial.Serial('COM3', 9600)
@@ -23,36 +22,25 @@ tiempo_lectura_minutos = 15  # Cambia el valor a la cantidad de minutos que dese
 # Obtiene el tiempo de inicio
 tiempo_inicio = time.time()
 
-
-df = pd.DataFrame(columns = ['Tiempo', 'Temperatura'])
-
+#Iniciamos un try - except para que se ejecute la lectura del puerto y si interrumpimos el kernel se guarden los datos y no se pierdan
 try:
     while (time.time() - tiempo_inicio) < (tiempo_lectura_minutos * 60):
         cad = serialArduino.readline().decode('ascii')
         print(cad)
-        num = cad.split(', ')
-        num = [float(numero) for numero in num]
-        new_row = pd.DataFrame({'Tiempo': [num[0]], 'Temperatura': [num[1]]})
-        df = pd.concat([df, new_row], ignore_index=True)
-        archivo.write(cad) #pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+        num = cad.split(', ') # Separamos los datos en los dos numeros
+        num = [float(numero) for numero in num] #Hacemos los numeros a flotantes
+        new_row = pd.DataFrame({'Tiempo': [num[0]], 'Temperatura': [num[1]]}) #Esta es la nueva columna 
+        df = pd.concat([df, new_row], ignore_index=True) #Agregamos los nuevos datos al dataframe
 except KeyboardInterrupt:
     print("Interrupción del usuario. Cerrando el puerto serial y guardando datos...")
 
 # Cierra el archivo y el puerto serial
-archivo.close()
 serialArduino.close()
 
-df_txt = pd.read_csv('C:\\Users\\jonat.JONATHANROJAS\\Desktop\\SENSOR_temperatura\\prueba.txt', sep = ',', header = None)
-##Cambiamos el nombre de las columnas para fácil identificación
-n_cols = ['Tiempo', 'Temperatura']
-df_txt.columns = n_cols
-columnas = list(df_txt.columns)
+df['Tiempo'] = df['Tiempo']/1000 # Cambiamos de milisegundos a segundos
+df.to_csv("Prueba_01_01.csv", index=False) #Guardamos los datos en csv
 
-
-df['Tiempo'] = df['Tiempo']/1000
-df.to_csv("Prueba_01_01.csv", index=False)
-
-
+## Graficamos los datos obtenidos
 plt.scatter(df['Tiempo'], df['Temperatura'], color = 'g')
 plt.xlabel('Tiempo ($s$)')
 plt.ylabel('Temperatura ($^{\circ}K$)')
