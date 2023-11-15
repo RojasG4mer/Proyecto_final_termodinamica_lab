7ea86af2ad2496cee2c27486a606f7630aeb7928
rojasg4mer <jonathan.frmipn@gmail.com>
2023-11-15 15:30:38 -0600

    Se agrega un archivo nuevo

--- Lectura_puerto_serial.py
+++ Lectura_puerto_serial.py
@@ -23,11 +23,18 @@ tiempo_lectura_minutos = 15  # Cambia el valor a la cantidad de minutos que dese
 # Obtiene el tiempo de inicio
 tiempo_inicio = time.time()
 
+
+df = pd.DataFrame(columns = ['Tiempo', 'Temperatura'])
+
 try:
     while (time.time() - tiempo_inicio) < (tiempo_lectura_minutos * 60):
         cad = serialArduino.readline().decode('ascii')
         print(cad)
-        archivo.write(cad)
+        num = cad.split(', ')
+        num = [float(numero) for numero in num]
+        new_row = pd.DataFrame({'Tiempo': [num[0]], 'Temperatura': [num[1]]})
+        df = pd.concat([df, new_row], ignore_index=True)
+        archivo.write(cad) #pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
 except KeyboardInterrupt:
     print("Interrupción del usuario. Cerrando el puerto serial y guardando datos...")
 
@@ -35,16 +42,18 @@ except KeyboardInterrupt:
 archivo.close()
 serialArduino.close()
 
-df = pd.read_csv('C:\\Users\\jonat.JONATHANROJAS\\Desktop\\SENSOR_temperatura\\prueba.txt', sep = ',', header = None)
+df_txt = pd.read_csv('C:\\Users\\jonat.JONATHANROJAS\\Desktop\\SENSOR_temperatura\\prueba.txt', sep = ',', header = None)
 ##Cambiamos el nombre de las columnas para fácil identificación
 n_cols = ['Tiempo', 'Temperatura']
-df.columns = n_cols
-columnas = list(df.columns)
+df_txt.columns = n_cols
+columnas = list(df_txt.columns)
 
 
 df['Tiempo'] = df['Tiempo']/1000
+df.to_csv("Prueba_01_01.csv", index=False)
 
-plt.scatter(df.iloc[:, 0], df.iloc[:, 1], color = 'g')
+
+plt.scatter(df['Tiempo'], df['Temperatura'], color = 'g')
 plt.xlabel('Tiempo ($s$)')
 plt.ylabel('Temperatura ($^{\circ}K$)')
 plt.title('Intento')
